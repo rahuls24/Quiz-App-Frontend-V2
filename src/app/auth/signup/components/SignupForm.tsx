e8f49e47784d69b42cd0add37e943288d7f89552
Rahul Kumar <cse.170310430@silicon.ac.in>
2023-11-08 14:11:21 +0530

    remove unused imports

--- src/app/auth/signup/components/SignupForm.tsx
+++ src/app/auth/signup/components/SignupForm.tsx
@@ -22,7 +22,6 @@ import TextField from '@mui/material/TextField';
 // Material-UI hooks
 import { useTheme } from '@mui/material';
 // Next.js
-import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
 // React
@@ -41,116 +40,118 @@ import useEmailSignupForm from '../hooks/useEmailSignupForm';
  * @returns {JSX.Element} The rendered SignupForm component.
  */
 function SignupForm() {
-  const [shouldShowPassword, setShowPassword] = useState(false);
-  const theme = useTheme();
-  const { formik, isLoading } = useEmailSignupForm();
-  const roleRadioBtns = (
-    <FormControl fullWidth>
-      <FormLabel id="role">Role</FormLabel>
-      <RadioGroup
-        aria-labelledby="role-radio-buttons-group"
-        name="role"
-        id="role"
-        value={formik.values.role}
-        row
-        sx={{ gap: 4 }}
-        onChange={formik.handleChange}
-      >
-        <FormControlLabel
-          value="examinee"
-          control={<Radio />}
-          label="Examinee"
-        />
-        <FormControlLabel
-          value="examiner"
-          control={<Radio />}
-          label="Examiner"
-        />
-      </RadioGroup>
-    </FormControl>
-  );
-  const endAdornmentForPassword = (
-    <InputAdornment position="end">
-      <IconButton
-        aria-label="toggle password visibility"
-        onClick={() => setShowPassword(!shouldShowPassword)}
-        onMouseDown={() => setShowPassword(!shouldShowPassword)}
-        edge="end"
-      >
-        {shouldShowPassword ? <Visibility /> : <VisibilityOff />}
-      </IconButton>
-    </InputAdornment>
-  );
-  return (
-    <Box component="form" onSubmit={formik.handleSubmit} sx={{ mt: 1 }}>
-      <TextField
-        margin="normal"
-        fullWidth
-        id="name"
-        label="Name"
-        name="name"
-        autoComplete="name"
-        value={formik.values.name}
-        onChange={formik.handleChange}
-        error={formik.touched.name && Boolean(formik.errors.name)}
-        helperText={formik.touched.name && formik.errors.name}
-      />
-      <TextField
-        margin="normal"
-        fullWidth
-        id="email"
-        label="Email Address"
-        name="email"
-        autoComplete="email"
-        value={formik.values.email}
-        onChange={formik.handleChange}
-        error={formik.touched.email && Boolean(formik.errors.email)}
-        helperText={formik.touched.email && formik.errors.email}
-      />
-      <TextField
-        margin="normal"
-        fullWidth
-        name="password"
-        label="Password"
-        type={shouldShowPassword ? 'text' : 'password'}
-        id="password"
-        InputProps={{
-          endAdornment: endAdornmentForPassword,
-        }}
-        autoComplete="current-password"
-        value={formik.values.password}
-        onChange={formik.handleChange}
-        error={formik.touched.password && Boolean(formik.errors.password)}
-        helperText={formik.touched.password && formik.errors.password}
-      />
-      {roleRadioBtns}
+	const [shouldShowPassword, setShowPassword] = useState(false);
+	const theme = useTheme();
+	const { formik, isLoading } = useEmailSignupForm();
+	const roleRadioBtns = (
+		<FormControl fullWidth>
+			<FormLabel id='role'>Role</FormLabel>
+			<RadioGroup
+				aria-labelledby='role-radio-buttons-group'
+				name='role'
+				id='role'
+				value={formik.values.role}
+				row
+				sx={{ gap: 4 }}
+				onChange={formik.handleChange}
+			>
+				<FormControlLabel
+					value='examinee'
+					control={<Radio />}
+					label='Examinee'
+				/>
+				<FormControlLabel
+					value='examiner'
+					control={<Radio />}
+					label='Examiner'
+				/>
+			</RadioGroup>
+		</FormControl>
+	);
+	const endAdornmentForPassword = (
+		<InputAdornment position='end'>
+			<IconButton
+				aria-label='toggle password visibility'
+				onClick={() => setShowPassword(!shouldShowPassword)}
+				onMouseDown={() => setShowPassword(!shouldShowPassword)}
+				edge='end'
+			>
+				{shouldShowPassword ? <Visibility /> : <VisibilityOff />}
+			</IconButton>
+		</InputAdornment>
+	);
+	return (
+		<Box component='form' onSubmit={formik.handleSubmit} sx={{ mt: 1 }}>
+			<TextField
+				margin='normal'
+				fullWidth
+				id='name'
+				label='Name'
+				name='name'
+				autoComplete='name'
+				value={formik.values.name}
+				onChange={formik.handleChange}
+				error={formik.touched.name && Boolean(formik.errors.name)}
+				helperText={formik.touched.name && formik.errors.name}
+			/>
+			<TextField
+				margin='normal'
+				fullWidth
+				id='email'
+				label='Email Address'
+				name='email'
+				autoComplete='email'
+				value={formik.values.email}
+				onChange={formik.handleChange}
+				error={formik.touched.email && Boolean(formik.errors.email)}
+				helperText={formik.touched.email && formik.errors.email}
+			/>
+			<TextField
+				margin='normal'
+				fullWidth
+				name='password'
+				label='Password'
+				type={shouldShowPassword ? 'text' : 'password'}
+				id='password'
+				InputProps={{
+					endAdornment: endAdornmentForPassword,
+				}}
+				autoComplete='current-password'
+				value={formik.values.password}
+				onChange={formik.handleChange}
+				error={
+					formik.touched.password && Boolean(formik.errors.password)
+				}
+				helperText={formik.touched.password && formik.errors.password}
+			/>
+			{roleRadioBtns}
 
-      <LoadingButton
-        loading={isLoading}
-        type="submit"
-        loadingPosition="end"
-        fullWidth
-        variant="contained"
-        sx={{ mt: 3, mb: 2 }}
-        endIcon={<ArrowRightIcon />}
-      >
-        Sign Up
-      </LoadingButton>
-      <Grid container>
-        <Grid item xs></Grid>
-        <Grid item>
-          <Link
-            href="/auth/signin"
-            style={{
-              color: theme.palette.primary.main ?? '#00BCD4',
-            }}
-          >
-            {'Already have an account? Sign In'}
-          </Link>
-        </Grid>
-      </Grid>
-    </Box>
-  );
+			<LoadingButton
+				loading={isLoading}
+				type='submit'
+				loadingPosition='end'
+				fullWidth
+				variant='contained'
+				sx={{ mt: 3, mb: 2 }}
+				endIcon={<ArrowRightIcon />}
+			>
+				Sign Up
+			</LoadingButton>
+			<Grid container>
+				<Grid item xs></Grid>
+				<Grid item>
+					<Link
+						href='/auth/signin'
+						style={{
+							color: theme.palette.primary.main ?? '#00BCD4',
+						}}
+					>
+						{'Already have an account? Sign In'}
+					</Link>
+				</Grid>
+			</Grid>
+		</Box>
+	);
 }
 
 export default SignupForm;
