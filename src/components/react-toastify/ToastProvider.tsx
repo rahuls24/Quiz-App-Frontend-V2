015821e250acceda6700ed8d96d42512d348900f
Rahul Kumar <cse.170310430@silicon.ac.in>
2023-11-08 10:25:49 +0530

    docs added

--- src/components/react-toastify/ToastProvider.tsx
+++ src/components/react-toastify/ToastProvider.tsx
@@ -1,15 +1,25 @@
 'use client';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+/**
+ * ToastProvider Component
+ *
+ * This component provides a toast notification container to display notifications in the application.
+ *
+ * @param {Object} props - The props object.
+ * @param {React.ReactNode} props.children - The child components to be rendered within the provider.
+ * @returns {JSX.Element} The rendered ToastProvider component.
+ */
 export default function ToastProvider({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <>
-      {children}
-      <ToastContainer />
-    </>
-  );
+	return (
+		<>
+			{children}
+			<ToastContainer />
+		</>
+	);
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,32 +1,41 @@
 'use client';
 import {
-  PreloadedState,
-  combineReducers,
-  configureStore,
+	PreloadedState,
+	combineReducers,
+	configureStore,
 } from '@reduxjs/toolkit';
 import globalSlice from '@/store/globalSlice';
 import { api } from './apiSetup';
 import { authenticationApi } from '../services/auth/auth';
 
 const rootReducer = combineReducers({
-  [api.reducerPath]: api.reducer,
-  [authenticationApi.reducerPath]: authenticationApi.reducer,
-  global: globalSlice,
-  //add all your reducers here
+	[api.reducerPath]: api.reducer,
+	[authenticationApi.reducerPath]: authenticationApi.reducer,
+	global: globalSlice,
+	//add all your reducers here
 });
 
+/**
+ * setupStore Function
+ *
+ * Sets up and configures the Redux store with the specified preloaded state.
+ *
+ * @param {PreloadedState<RootState>} preloadedState - The preloaded state for the Redux store (optional).
+ * @returns {Store<RootState>} The configured Redux store.
+ */
 export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
-  return configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) =>
-      // adding the api middleware enables caching, invalidation, polling and other features of `rtk-query`
-      getDefaultMiddleware().concat([
-        api.middleware,
-        authenticationApi.middleware,
-      ]),
-    preloadedState,
-  });
+	return configureStore({
+		reducer: rootReducer,
+		middleware: (getDefaultMiddleware) =>
+			// adding the api middleware enables caching, invalidation, polling and other features of `rtk-query`
+			getDefaultMiddleware().concat([
+				api.middleware,
+				authenticationApi.middleware,
+			]),
+		preloadedState,
+	});
 };
+
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = AppStore['dispatch'];
